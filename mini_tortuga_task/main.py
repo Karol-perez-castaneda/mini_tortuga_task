9c05456c4f243889b9a4b06e0d57fdca519dec36
Karol Liceth Perez Castañeda <karol.perez@est.iudigital.edu.co>
2025-12-14 09:51:37 -0500

    Delete mini_tortuga_task directory

--- mini_tortuga_task/main.py
+++
@@ -1,9 +0,0 @@
-from mini_tortuga import adelante, abajo, reiniciar
-from mini_tortuga import drawer_logic
-
-adelante(10)
-abajo(5)
-
-reiniciar(0)
-adelante(5)
-abajo(8)
--- mini_tortuga_task/mini_tortuga/__init__.py
+++
@@ -1,8 +0,0 @@
-
-from .drawer_logic import (
-    adelante, 
-    abajo, 
-    reiniciar
-)
-__version__ = "1.0.0"
-__all__ = ["adelante", "abajo", "reiniciar"]
